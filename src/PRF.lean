40fea8856541e9d551306ae0d83402f2fc209583
bruno cuconato (@odanoburu) <bcclaro@gmail.com>
2022-10-05 16:19:13 -0300

    Update examples and remove some unused stuff

--- src/PRF.lean
+++ src/PRF.lean
@@ -9,48 +9,21 @@ structure Vector (n : Nat) (α : Type u) where
     -- List.length xs.val = n, n <= n → n <= List.length xs.val
     List.get xs.val {val := i, isLt := Nat.le_trans isLt (Eq.subst (Eq.symm xs.hasLen) Nat.le.refl)}
 
-def Vector.fromList {α : Type u} : (as : List α) → Vector (List.length as) α
+@[specialize] def Vector.fromList {α : Type u} : (as : List α) → Vector (List.length as) α
   | xs => {val := xs, hasLen := rfl}
 
--- @[specialize] def Vector.cons {α : Type u} : α → Vector n α → Vector (n + 1) α
---   | x, {val := xs, hasLen} =>
---     let consLen : List.length (x :: xs) = List.length xs + 1 := List.length_cons x xs
---     {val := x :: xs, hasLen := @Eq.subst _ (λ sm => List.length (x :: xs) = sm + 1) _ _ hasLen consLen}
-
-def Vector.enumeration {α : Type u} : Vector n α → (Fin n → α)
+@[specialize] def Vector.enumeration {α : Type u} : Vector n α → (Fin n → α)
   | as => λ i => Vector.get as i
-
--- @[specialize] def List.build {α : Type u} (n : Nat) (f : Fin (n + 1) → α) : List α :=
---   let rec helper : Fin (n + 1)  → List α → List α
---     | {val := 0, isLt := _}, acc => (f {val := 0, isLt := Nat.zero_lt_succ n} :: acc)
---     | {val := i + 1, isLt := succ_i_lt_n}, acc =>
---       let i_lt_succ_n := Nat.lt_trans Nat.le.refl succ_i_lt_n
---       helper {val := i, isLt := i_lt_succ_n} (f {val := i + 1, isLt := succ_i_lt_n} :: acc)
---   helper {val := n, isLt := Nat.le.refl} []
-
--- @[simp] theorem List.length_build {α} (n : Nat) {f : Fin (n + 1) → α} : Eq (List.build n f).length (n + 1) := by
---   induction n with
---   | zero => rfl
---   | succ n => sorry
-
--- @[specialize] def Vector.build {α : Type u} (n : Nat) (f : Fin (n + 1) → α) : Vector (n + 1) α :=
---   {val := (List.build n f), hasLen := List.length_build n}
 
 namespace PRF
 inductive PRF : Nat → Type where
   | zero    : PRF 0
   | succ    : PRF 1
   | proj    : {n : Nat} → Fin n → PRF n -- projection
-  | comp    : {m k : Nat} → PRF m → (Fin m → PRF k) → PRF k -- composition
+  -- composition: f(x) = g(h₁(x*),...,hₘ(x*)), where arity(f) = k, arity(g) = m, arity(hᵢ) = k
+  | comp    : {m k : Nat} → PRF m → (Fin m → PRF k) → PRF k
+  -- primitive recursion: f(0, x*) = g(x*); f(n + 1, x*) = h(f(n, x*), n, x*)
   | primrec : {n : Nat} → PRF n → PRF (n + 2) → PRF (n + 1) -- primitive recursion
-
---. composition:
--- If g is a function of m-variables and h₁ ... , hₘ are functions of k variables,
--- which are already defined, then composition yields the function
--- f(x) = g(h₁(x*),...,hₘ(x*))
---. primitive recursion:
--- f(0, x*) = g(x*)
--- f(n + 1, x*) = h(f(n, x*), n, x*)
 
 def PRF.ToString : PRF n → String
   | PRF.zero => "zero"
@@ -146,15 +119,15 @@ def PRF.add : PRF 2 :=
     PRF.identity
     (PRF.comp1 PRF.succ PRF.first)
 
--- #eval evaluate 1000 PRF.add [3, 2]
+--#eval eval PRF.add (Vector.fromList [3, 2])
 
 def PRF.mul : PRF 2 :=
   PRF.primrec
     (PRF.const 0)
     (PRF.comp2 PRF.add (PRF.proj 0) (PRF.proj 2))
 
--- #eval evaluate 1000 PRF.mul [3, 0]
--- #eval evaluate 1000 PRF.mul [3, 4]
+--#eval eval PRF.mul (Vector.fromList [3, 0])
+--#eval eval PRF.mul (Vector.fromList [3, 4])
 
 def PRF.exp : PRF 2 :=
   PRF.comp2
@@ -164,24 +137,24 @@ def PRF.exp : PRF 2 :=
     PRF.second
     PRF.first
 
--- #eval evaluate 100000000 PRF.exp [0, 0]
--- #eval evaluate 100000000 PRF.exp [0, 1]
--- #eval evaluate 100000000 PRF.exp [10, 0]
---#eval evaluate 100000000 PRF.exp [10, 2]
+-- #eval eval PRF.exp (Vector.fromList [0, 0])
+-- #eval eval PRF.exp (Vector.fromList [0, 1])
+-- #eval eval PRF.exp (Vector.fromList [10, 0])
+--#eval eval PRF.exp (Vector.fromList [10, 2])
 
 def PRF.pred : PRF 1 :=
   PRF.primrec (PRF.const 0) PRF.second
 
--- #eval evaluate 100000000 PRF.pred [2]
+-- #eval eval PRF.pred (Vector.fromList [2])
 
 def PRF.signal : PRF 1 :=
   PRF.primrec
     (PRF.const 0)
     (PRF.const 1)
 
--- #eval evaluate 100 PRF.signal [0]
--- #eval evaluate 100 PRF.signal [1]
--- #eval evaluate 100 PRF.signal [20]
+--#eval eval PRF.signal (Vector.fromList [0])
+--#eval eval PRF.signal (Vector.fromList [1])
+-- #eval eval PRF.signal (Vector.fromList [20])
 
 def PRF.le : PRF 2 :=
   PRF.comp1 PRF.signal
@@ -189,16 +162,16 @@ def PRF.le : PRF 2 :=
       (PRF.comp1 PRF.succ PRF.first)
       (PRF.comp1 PRF.pred PRF.first))
 
--- #eval evaluate 100 PRF.le [9, 10]
--- #eval evaluate 100 PRF.le [10, 10]
--- #eval evaluate 100 PRF.le [2, 1]
+-- #eval eval PRF.le (Vector.fromList [9, 10])
+-- #eval eval PRF.le (Vector.fromList [10, 10])
+-- #eval eval PRF.le (Vector.fromList [2, 1])
 
 def PRF.lt : PRF 2 :=
   PRF.comp2 PRF.le (PRF.comp1 PRF.succ PRF.first) PRF.second
 
---#eval evaluate 100 PRF.lt [9, 10]
---#eval evaluate 100 PRF.lt [10, 10]
---#eval evaluate 100 PRF.lt [2, 1]
+--#eval eval PRF.lt (Vector.fromList [9, 10])
+--#eval eval PRF.lt (Vector.fromList [10, 10])
+--#eval eval PRF.lt (Vector.fromList [2, 1])
 
 def PRF.not : PRF 1 :=
   PRF.primrec
@@ -212,7 +185,7 @@ def PRF.if : PRF k → PRF k → PRF k → PRF k
       (PRF.comp2 PRF.mul (PRF.comp1 PRF.signal t) f)
       (PRF.comp2 PRF.mul (PRF.comp1 PRF.not t) g)
 
--- #eval evaluate 10 (PRF.if PRF.first PRF.first PRF.not) [0]
+--#eval eval (PRF.if PRF.first PRF.first PRF.not) (Vector.fromList [0])
 
 def PRF.or : PRF 2 :=
   PRF.comp1 PRF.signal (PRF.comp2 PRF.add PRF.first PRF.second)
@@ -220,7 +193,7 @@ def PRF.or : PRF 2 :=
 def PRF.and : PRF 2 :=
   PRF.comp2 PRF.mul (PRF.comp1 PRF.signal PRF.first) (PRF.comp1 PRF.signal PRF.second)
 
--- #eval evaluate 100 PRF.and [2, 0]
+-- #eval eval PRF.and (Vector.fromList [2, 0])
 
 def PRF.eq : PRF 2 :=
   PRF.comp2
@@ -228,9 +201,9 @@ def PRF.eq : PRF 2 :=
     PRF.le
     (PRF.comp2 PRF.le PRF.second PRF.first)
 
---#eval evaluate 100000 PRF.eq [0, 1]
---#eval evaluate 100000 PRF.eq [11, 11]
---#eval evaluate 100000 PRF.eq [2, 1]
+--#eval eval PRF.eq (Vector.fromList [0, 1])
+--#eval eval PRF.eq (Vector.fromList [11, 11])
+--#eval eval PRF.eq (Vector.fromList [2, 1])
 
 def PRF.fixFirst : PRF k → PRF (k + 1) → PRF k
   | z, f =>
@@ -264,7 +237,7 @@ def PRF.dropNth : Nat → PRF k → PRF (k + 1)
                  , isLt := Nat.succ_le_succ iLt
                  })
 
---#eval evaluate 100 (PRF.dropNth 0 PRF.le) [4, 5 , 3]
+--#eval eval (PRF.dropNth 0 PRF.le) (Vector.fromList [4, 5 , 3])
 
 def PRF.mapNth : Nat → PRF k → PRF k → PRF k
   | i, g, f =>
@@ -300,22 +273,7 @@ def PRF.firstLEsat : PRF (k + 1) → PRF k → PRF k
             (PRF.comp1 PRF.succ <| PRF.comp1 PRF.succ PRF.second)
             (PRF.const 0)))))
 
---#eval evaluate 1000 (@PRF.firstLEsat 0 (PRF.comp2 PRF.le (PRF.const 5) PRF.first) (PRF.const 10)) [1]
-
-def PRF.anyLEsat : PRF 1 → PRF 1
--- anyLEsat(p, n) = { 1 if p(i) for some i <= n; 0 otherwise}
-  | p =>
-    (PRF.primrec
-      (PRF.comp1 PRF.signal (PRF.comp1 p (PRF.const 0)))
-      (PRF.if
-        PRF.first
-        PRF.first
-        (PRF.if
-          (PRF.comp1 p (PRF.comp1 PRF.succ PRF.second))
-          (PRF.const 1)
-          PRF.first)))
-
---#eval evaluate 1000 (PRF.anyLEsat (PRF.comp2 PRF.le (PRF.const 3) PRF.first)) [2]
+--#eval eval (@PRF.firstLEsat 0 (PRF.comp2 PRF.le (PRF.const 5) PRF.first) (PRF.const 10)) (Vector.fromList [1])
 
 def PRF.disjunction : PRF (k + 1) → PRF (k + 1)
 -- disjunction(p, n, x₁, ..., xₙ) = { 1 if p(i, n, x₁, ..., xₙ) for some i <= n; 0 otherwise}
@@ -331,18 +289,17 @@ def PRF.disjunction : PRF (k + 1) → PRF (k + 1)
             (PRF.dropNth 0 p))))
       p
 
---#eval evaluate 1000 (@PRF.disjunction 0 (PRF.comp2 PRF.le (PRF.const 13) PRF.first)) [0]
+--#eval eval (@PRF.disjunction 0 (PRF.comp2 PRF.le (PRF.const 13) PRF.first)) (Vector.fromList [0])
 
 def PRF.limMin : PRF (k + 1) → PRF k → PRF k
   -- (μ z ≤ g(x₁, ..., xₙ))[h(z, x₁, ..., xₙ) > 0]
   | h, g => PRF.firstLEsat h g
 
--- #eval evaluate
---         1000
+-- #eval eval
 --         (PRF.limMin
 --           (PRF.comp2 PRF.le (PRF.const 3) PRF.first)
 --           (@PRF.comp2 1 PRF.add PRF.first (PRF.const 1)))
---         [2]
+--         (Vector.fromList [2])
 
 namespace TM
 
@@ -517,7 +474,7 @@ def TM.concat : PRF 2 :=
         (PRF.comp1 len PRF.second)))
     PRF.second
 
---#eval evaluate 100000 (TM.concat 10) [1, 1]
+--#eval eval (TM.concat 10) (Vector.fromList [1, 1])
 
 def TM.pre : PRF 2 :=
   -- pre(w₁, w) = z s.t. ∃z,∃i, z.w₁.i = w
@@ -572,7 +529,7 @@ def TM.substring : PRF 2 :=
     PRF.second
     PRF.first
 
---#eval eval TM.substring (Vector.fromList [1, 12])
+--#eval eval TM.pre (Vector.fromList [2, 11])
 
 def TM.subst : PRF 3 :=
   -- subst(w₁, w₂, w) = if substring(w₁, w) then w[w₁ ← w₂] else w
@@ -584,7 +541,7 @@ def TM.subst : PRF 3 :=
       (PRF.comp2 concat PRF.second (PRF.comp2 post PRF.first PRF.third)))
     PRF.third
 
---#eval evaluate 100000 (TM.subst 10) [2, 1, 121]
+--#eval eval (TM.subst 10) (Vector.fromList [2, 1, 121])
 
 def TM.state : PRF 1 :=
   -- get current TM state
